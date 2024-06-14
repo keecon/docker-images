1a5e2ab15572f4942fbe87e418fee15872689040
iwaltgen <iwaltgen@gmail.com>
2024-06-14 10:59:46 +0900

    fix: 사용자 초기화 스크립트 없는 경우 재시작 하지 않도록 수정

--- mssql/init.d/setup.sql
+++ mssql/init.d/setup.sql
@@ -1,3 +1,12 @@
+USE master;
+GO
+
+RAISERROR('NOT EXISTS USER SETUP SCRIPT', 20, -1) WITH LOG
+
 /*
 Enter custom T-SQL here that would run after SQL Server has started up.
+
+IF EXISTS (SELECT name FROM sys.databases WHERE name = '$DATABASE_NAME') BEGIN
+	RAISERROR('ALREADY EXISTS DATABASE: $DATABASE_NAME', 20, -1) WITH LOG
+END
 */
